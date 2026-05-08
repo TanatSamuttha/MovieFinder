a0e84810ddc99ffd7802a2723d655110a7a8553f
Tanat Samuttha <163229612+TanatSamuttha@users.noreply.github.com>
2026-05-09 00:21:06 +0700

    show profile after login

--- services/gateway/static/app.js
+++ services/gateway/static/app.js
@@ -1,7 +1,36 @@
-import { authen } from "./js/auth.js";
+import { authen, logout } from "./js/auth.js";
 
-const signinBtn = document.getElementById("login-btn");
+const themeBtn = document.getElementById('theme-toggle');
+const loginBtn = document.getElementById('login-btn');
+const logoutBtn = document.getElementById('logout-btn');
+const userInfo = document.getElementById('user-info');
+const userProfile = document.getElementById('user-profile');
+const toast = document.getElementById('toast');
+const movieGrid = document.getElementById('movie-grid');
+const favSection = document.getElementById('favorites-section');
+const favGrid = document.getElementById('favorites-grid');
+const clearFavBtn = document.getElementById('clear-fav-btn'); 
 
-signinBtn.addEventListener("click", async () => {
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-btn');
+const sectionTitle = document.getElementById('section-title');
+const paginationWrapper = document.getElementById('pagination-wrapper');
+
+loginBtn.addEventListener("click", async () => {
     const data = await authen();
+    if(!data) return;
+    const user = data.user;
+    console.log(JSON.stringify(user));
+    loginBtn.classList.add('hidden');
+    userProfile.classList.remove('hidden');
+    userInfo.textContent = `Hi, ${user.displayName.split(' ')[0]}`;
+    favSection.classList.remove('hidden');
+})
+
+logoutBtn.addEventListener("click", async () => {
+    await logout();
+    loginBtn.classList.remove('hidden');
+    userProfile.classList.add('hidden');
+    userInfo.textContent = `Hi, ${user.displayName.split(' ')[0]}`;
+    favSection.classList.add('hidden');
 })
